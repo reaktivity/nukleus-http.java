957f0edbea8e27e8b7960e170f4dad5f82977287
Chris Barrow <chris.barrow@kaazing.com>
2017-06-05 10:54:34 -0800

    Fixed a bug in SourceInputStream.decodeHttpHeaders

--- src/main/java/org/reaktivity/nukleus/http/internal/routable/stream/SourceInputStreamFactory.java
+++ src/main/java/org/reaktivity/nukleus/http/internal/routable/stream/SourceInputStreamFactory.java
@@ -790,6 +790,7 @@ public final class SourceInputStreamFactory
                     else
                     {
                         isChunkedTransfer = true;
+                        headers.put(name, value);
                     }
 
                 }
